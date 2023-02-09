a551e136408cd227c12d0e22191dd6d820b40030
Kate Demchuk <kate.v.demchuk@gmail.com>
2023-02-10 00:04:43 +0100

    Initial style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
     <div className="App">
       <header className="App-header">
       </header>
+
+      <h1>Cyber dilemma</h1>
+
+      <p>
+        Live a day of a <span class="text-accent">cyber hacker's life</span> and face a <span class="text-danger">challenge</span>. You will be required to make multiple decisions which will determine a game ending. <span class="text-idea">Each decision affects where you finish your path.</span>
+      </p>
+
+      <a>Press SPACE to start</a>
     </div>
   );
 }
