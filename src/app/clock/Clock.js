d5c4d7822adfe1e8ebfca0673cb481c9a518d7e0
Kate Demchuk <kate.v.demchuk@gmail.com>
2023-02-10 12:05:00 +0100

    Make Clock dynamic

--- src/app/clock/Clock.js
+++ src/app/clock/Clock.js
@@ -1,14 +1,25 @@
 import './Clock.css';
+import {useState, useEffect} from 'react';
 import {MONTHS} from '../../constants/months';
 import {DAYS} from '../../constants/days';
 
 function Clock() {
-  const now = new Date();
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setNow(t => new Date());
+    }, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
   const day = now.getDay();
   const date = now.getDate();
   const month = now.getMonth();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
+  const hours = (now.getHours() + '').padStart(2, '0');
+  const minutes = (now.getMinutes() + '').padStart(2, '0');
 
   return (
     <div className="Clock">{DAYS[day]} {date} {MONTHS[month]} {hours}:{minutes}</div>
